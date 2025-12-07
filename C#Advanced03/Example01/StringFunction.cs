0255189ccf834cff3de4bfc983de0078b6ca3303
george.amin <george.amin@GEORGE-AMIN>
2025-12-07 14:02:30 +0200

    Delegate

---
+++ C#Advanced03/Example01/StringFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced03.Example01
+{
+    internal static class StringFunction
+    {
+        public static int GetCountOfUpperCaseChar(string? str)
+        {
+            // function will return the counter of chars if upperCase
+            int Counter = 0;
+            if (!string.IsNullOrWhiteSpace(str)) // without ! will retnr true if there is null or white Space
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i]))
+                    {
+                        Counter++;
+                    }
+                }
+
+            }
+            return Counter;
+        }
+
+    }
+}
--- C#Advanced03/Program.cs
+++ C#Advanced03/Program.cs
@@ -1,14 +1,46 @@
-﻿namespace C_Advanced03
+﻿using C_Advanced03.Example01;
+using System.Diagnostics;
+
+namespace C_Advanced03
 {
     internal class Program
     {
+        //public static void Print()
+        //{
+        //    Console.WriteLine("I am print method");
+        //}
+
+
+        public delegate int StringFunc(string? s);   
+
         static void Main(string[] args)
         {
             #region Delegate
-                
+            // ...use to 
+            // 1. Implement function programming  paradigm = make functoins return function or send function as a prametare in functions
+            // 2. Implement Event-Driven progamming Paradigm = Like a treger function when action fire the event will work
+            //Print();
+            //int x = Program.Print(); // Invalid
+            //var x = Program.Print(); // Invalid
+
+            #region EX01
+            //Console.WriteLine("Eter your Name");
+            //string Name = Console.ReadLine();
+            //string name ="GEORGE";
+
+            //Console.WriteLine(StringFunction.GetCountOfUpperCaseChar(name));
+
+
+
+            #endregion
+
+
 
 
             #endregion
         }
     }
 }
+
+
+
